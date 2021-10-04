62801a5520431846f6000d04f18d836a614d30c8
rayhan13ads <rayhan13ads@gmail.com>
2021-10-04 17:53:16 +0600

    server setup task ongoing

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var http = require('http');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;  
+var server = http.createServer(app);
+
+var io = require('socket.io')(server,{
+    cors:{
+        origin: '*'
+    }
+})
+
+app.use(express.json());
+app.use(cors());
+
+io.on("Connection",(socket)=>{
+    console.log("connected");
+})
+
+server.listen(port,()=>{
+    console.log("server running");
+});
